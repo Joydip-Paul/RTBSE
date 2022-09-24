9d3d2f8e0d43699a3696a4ed30ef74d28c899e69
Joydip Paul <joydippaul72@gmail.com>
2022-09-25 02:14:34 +0600

    typing master

--- src/app/components/typing-master/typing-master.component.ts
+++ src/app/components/typing-master/typing-master.component.ts
@@ -6,10 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./typing-master.component.scss']
 })
 export class TypingMasterComponent implements OnInit {
-
+  text = "According to board officials and the police, the question papers were leaked from the Nehal Uddin Pilot Girls' High School centre in Bhurungamari. Police on Wednesday arrested three teachers of the school for their alleged involvement in leaking the question papers.";
+  enteredText = "";
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  typeOnchange(value: string) {
+    console.log(value);
+    this.enteredText = value;
+  }
+
+  compare(singleLetter: string, enteredText: string) {
+    if (!enteredText) {
+      return "pending";
+    }
+    if (enteredText === singleLetter) {
+      return "success";
+    }
+    else {
+      return "error";
+    }
   }
 
 }
